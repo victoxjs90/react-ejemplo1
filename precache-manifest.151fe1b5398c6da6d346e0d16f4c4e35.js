dcdd09eb6eea8db3a7899b9bb520f5d81fb04b71
victor avendano <victor.eduardo.adm.99@gmail.com>
2020-09-16 15:10:17 -0300

    Updates

---
+++ precache-manifest.151fe1b5398c6da6d346e0d16f4c4e35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b114f994c36f296762d97e28ae3ebb",
+    "url": "/react-ejemplo1/index.html"
+  },
+  {
+    "revision": "b8224cda390f0044c129",
+    "url": "/react-ejemplo1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ab6e1269ecf7ce85b59",
+    "url": "/react-ejemplo1/static/js/2.cc28d733.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ejemplo1/static/js/2.cc28d733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8224cda390f0044c129",
+    "url": "/react-ejemplo1/static/js/main.60390af3.chunk.js"
+  },
+  {
+    "revision": "c24f1b24bb369f546ed9",
+    "url": "/react-ejemplo1/static/js/runtime-main.15c00f76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ejemplo1/static/media/logo.5d5d9eef.svg"
+  }
+]);
